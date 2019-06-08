b6ebcb8f35017a5d26a9806e728670a59cdf4be8
fwcd <fwcdmail@gmail.com>
2019-06-08 02:37:02 +0200

    Specify default configuration in LoggingManager
    Use the console backend and the info level as defaults in
    LoggingManager.

--- src/LoggingManager.ts
+++ src/LoggingManager.ts
@@ -2,6 +2,8 @@ import { Logger } from "./logger/Logger";
 import { DefaultLogger } from "./logger/DefaultLogger";
 import { HierarchialLoggerConfiguration } from "./logger/HierarchialLoggerConfiguration";
 import { LoggerConfiguration } from "./logger/LoggerConfiguration";
+import { ConsoleBackend } from "./backend/ConsoleBackend";
+import { LogLevel } from "./logger/LogLevel";
 
 /**
  * Creates loggers and manages a configuration
@@ -15,10 +17,33 @@ import { LoggerConfiguration } from "./logger/LoggerConfiguration";
 export class LoggingManager {
 	readonly config: LoggerConfiguration;
 	
+	/**
+	 * Creates a new logging managers with
+	 * (optionally) a custom set of parameters.
+	 * Any parameters that are left unspecified
+	 * are substituted with the default values.
+	 * 
+	 * @param config The custom configuration entries
+	 */
 	constructor(config?: LoggerConfiguration) {
-		this.config = config || {};
+		// Setup default configuration
+		this.config = {
+			name: "Logger",
+			backend: new ConsoleBackend(),
+			level: LogLevel.Info
+		};
+		// If specified, assign custom configuration entries
+		if (config) {
+			Object.assign(this.config, config);
+		}
 	}
 	
+	/**
+	 * Fetches a named logger that inherits
+	 * the configuration of this manager.
+	 * 
+	 * @param name The name of the logger
+	 */
 	loggerWithName(name: string): Logger {
 		const subConfig = new HierarchialLoggerConfiguration(this.config);
 		subConfig.name = name;
